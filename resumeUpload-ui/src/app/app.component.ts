c5d9f2eb74bbe56b4e477ac51dd01f818a17e6d1
Subrat <mastersubrat007@gmail.com>
2020-11-12 22:01:19 +0530

    Progress bar fix

--- resumeUpload-ui/src/app/app.component.ts
+++ resumeUpload-ui/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from "@angular/core";
-import { HttpClient, HttpHeaders, HttpRequest, HttpEventType } from "@angular/common/http";
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpRequest,
+  HttpEventType,
+} from "@angular/common/http";
 
 declare const socket: any;
 // const socket = io("http://localhost:3000");
@@ -7,7 +12,7 @@ declare const socket: any;
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
   title = "resumeUpload-ui";
@@ -19,14 +24,12 @@ export class AppComponent implements OnInit {
   color = "primary";
   mode = "determinate";
   value = 50.25890809809;
-  socket = 'http';
+  socket = "http";
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {
-   
-  }
+  ngOnInit() {}
 
-  goToLink(url: string){
+  goToLink(url: string) {
     window.open(url, "_blank");
   }
 
@@ -39,50 +42,56 @@ export class AppComponent implements OnInit {
   resumableUpload() {
     let fileId = `${this.selectedFile.name}-${this.selectedFile.lastModified}`;
     let headers = new HttpHeaders({
-      "size": this.selectedFile.size.toString(),
+      size: this.selectedFile.size.toString(),
       "x-file-id": fileId,
-      'name': this.name
+      name: this.name,
     });
 
     this.http
       .get("http://localhost:3000/status", { headers: headers })
       .subscribe((res: any) => {
         console.log(JSON.stringify(res));
-        if(res.status === 'file is present'){
+        if (res.status === "file is present") {
           alert(res.status);
           return;
         }
         let uploadedBytes = res.uploaded;
+        console.log(uploadedBytes);
         let headers2 = new HttpHeaders({
-          "size": this.selectedFile.size.toString(),
+          size: this.selectedFile.size.toString(),
           "x-file-id": fileId,
           "x-start-byte": uploadedBytes.toString(),
-          'name': this.name
+          name: this.name,
         });
-        const req = new HttpRequest('POST', "http://localhost:3000/upload", this.selectedFile.slice(uploadedBytes, this.selectedFile.size +1 ),{
-           headers: headers2,
-          reportProgress: true
-        });
-        this.http.request(req)
-          .subscribe(
-            (res: any) => {
-              if(res.type === HttpEventType.UploadProgress){
-                this.uploadPercent = Math.round(100* uploadedBytes/this.selectedFile.size);
-                console.log(this.uploadPercent);
-                if(this.uploadPercent >= 100){
-                  this.name = "";
-                  this.selectedFile = null;
-                }
-              }else{
-                console.log(JSON.stringify(res));
-                if(this.uploadPercent >= 100){
-                  this.name = "";
-                  this.selectedFile = null;
-                }
+        const req = new HttpRequest(
+          "POST",
+          "http://localhost:3000/upload",
+          this.selectedFile.slice(uploadedBytes, this.selectedFile.size + 1),
+          {
+            headers: headers2,
+            reportProgress: true,
+          }
+        );
+        this.http.request(req).subscribe(
+          (res: any) => {
+            if (res.type === HttpEventType.UploadProgress) {
+              this.uploadPercent = Math.round(
+                100 * ((res.loaded + uploadedBytes) / this.selectedFile.size)
+              );
+              if (this.uploadPercent >= 100) {
+                this.name = "";
+                this.selectedFile = null;
               }
-            },
-            err => {}
-          );
+            } else {
+              console.log(JSON.stringify(res));
+              if (this.uploadPercent >= 100) {
+                this.name = "";
+                this.selectedFile = null;
+              }
+            }
+          },
+          (err) => {}
+        );
       });
   }
 }
